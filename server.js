303b488c5fc21259284c52f60a5f75aea2577040
deepverma0 <dipanshuv010@gmail.com>
2026-04-21 19:34:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-*const razorpay = new Razorpay({
+const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET
 }); 
